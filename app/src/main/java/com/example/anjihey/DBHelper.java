9b101317adb7b095f3264ea7a2bcb1b2700188d6
DESKTOP-184M3H8\안지혜 <anjihey77@gamil.com>
2017-12-05 03:40:38 +0900

    안드로이드 2차과제

--- app/src/main/java/com/example/anjihey/DBHelper.java
+++ app/src/main/java/com/example/anjihey/DBHelper.java
@@ -7,7 +7,6 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    // DBHelper 생성자로 관리할 DB 이름과 버전 정보를 받음
     public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
@@ -19,7 +18,6 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL("CREATE TABLE FAVOTHING (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER, color TEXT, food TEXT);");
     }
 
-    // DB 업그레이드를 위해 버전이 변경될 때 호출되는 함수
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
@@ -28,7 +26,6 @@ public class DBHelper extends SQLiteOpenHelper {
     public void insert(String name, int age, String color, String food) {
         // 읽고 쓰기가 가능하게 DB 열기
         SQLiteDatabase db = getWritableDatabase();
-        // DB에 입력한 값으로 행 추가
         db.execSQL("INSERT INTO FAVOTHING VALUES(null, '" + name + "', " + age + ", '" + color + "', '" + food + "');");
         db.close();
     }
@@ -46,7 +43,7 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     public String getResult() {
-        // 읽기가 가능하게 DB 열기
+
         SQLiteDatabase db = getReadableDatabase();
         String result = "";
 
